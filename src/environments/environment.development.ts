1aedbb5ce8d41774a754cd5ea3f2c425480a74e9
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-21 14:07:55 +0000

    Setup and initialized firebase ( hosting, auth, firestore)

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,9 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'organic-shop-angular-ebe7a',
+    appId: '1:707236712347:web:8f38965bb2b99d42485b3e',
+    storageBucket: 'organic-shop-angular-ebe7a.appspot.com',
+    apiKey: 'AIzaSyBlxORnDAbtWRvmnV1dgRQJsIeBWVQybS8',
+    authDomain: 'organic-shop-angular-ebe7a.firebaseapp.com',
+    messagingSenderId: '707236712347',
+  },};
