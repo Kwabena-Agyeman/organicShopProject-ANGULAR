60ead5240863d40bc149805a876fd36071a9493b
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-21 17:39:09 +0000

    setup login with google and logout

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Auth, signOut } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-bs-navbar',
   templateUrl: './bs-navbar.component.html',
-  styleUrls: ['./bs-navbar.component.css']
+  styleUrls: ['./bs-navbar.component.css'],
 })
 export class BsNavbarComponent {
+  constructor(private auth: Auth) {}
 
+  async log0ut() {
+    signOut(this.auth);
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {
+  Auth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private auth: Auth) {}
 
+  googleProvider = new GoogleAuthProvider();
+
+  async login() {
+    signInWithPopup(this.auth, this.googleProvider);
+  }
 }
