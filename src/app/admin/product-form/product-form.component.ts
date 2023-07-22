0caf273a4050c9f344348ab6d372c7d2b628e6af
Kwabena Agyeman <kagyeman@ymail.com>
2023-07-22 19:21:44 +0000

    Set up categories service to fetch catgories from firebase

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DocumentData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { CategoryService } from 'src/app/category.service';
 
 @Component({
   selector: 'app-product-form',
   templateUrl: './product-form.component.html',
-  styleUrls: ['./product-form.component.css']
+  styleUrls: ['./product-form.component.css'],
 })
-export class ProductFormComponent {
+export class ProductFormComponent implements OnInit {
+  categories$: DocumentData[] = [];
 
+  constructor(private categoryService: CategoryService) {}
+  async ngOnInit(): Promise<void> {
+    this.categories$ = await this.categoryService.getCategories();
+  }
 }
---
+++ src/app/category.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  DocumentData,
+  Firestore,
+  collection,
+  getDocs,
+} from '@angular/fire/firestore';
+import { from, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  constructor(private db: Firestore) {}
+
+  async getCategories() {
+    console.log('called');
+    try {
+      const collectionRef = collection(this.db, 'categories');
+      console.log('called-2');
+
+      const categories: DocumentData[] = [];
+
+      const querySnapshot = await getDocs(collectionRef);
+      console.log({ querySnapshot });
+      querySnapshot.forEach((category) => {
+        categories.push(category.data());
+      });
+
+      console.log({ categories });
+      return categories;
+    } catch (error) {
+      return [];
+    }
+  }
+}
